c9cb235f593b86a8e2587f2d9282bfcfe6ef242a
Desiders <abc_desi@proton.me>
2023-07-16 02:03:00 +0300

    Change uow trait and sqlx impl

--- src/application/common/traits/uow.rs
+++ src/application/common/traits/uow.rs
@@ -2,12 +2,23 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait UnitOfWork {
+    type BeginError;
     type CommitError;
     type RollbackError;
 
-    /// Commits this transaction or savepoint, persisting any changes it has made.
-    async fn commit(self) -> Result<(), Self::CommitError>;
+    type Connection<'a>
+    where
+        Self: 'a;
 
-    /// Aborts this transaction or savepoint and rolls back any changes it has made.
-    async fn rollback(self) -> Result<(), Self::RollbackError>;
+    /// Gets a connection of this transaction or savepoint
+    fn connection(&mut self) -> Self::Connection<'_>;
+
+    /// Begins a new transaction or savepoint and returns a connection to it
+    async fn begin(&'static mut self) -> Result<Self::Connection<'_>, Self::BeginError>;
+
+    /// Commits this transaction or savepoint, persisting any changes it has made
+    async fn commit(&mut self) -> Result<(), Self::CommitError>;
+
+    /// Aborts this transaction or savepoint and rolls back any changes it has made
+    async fn rollback(&mut self) -> Result<(), Self::RollbackError>;
 }
--- src/infrastructure/database/uow.rs
+++ src/infrastructure/database/uow.rs
@@ -1,25 +1,62 @@
+use crate::application::common::traits::UnitOfWork;
+
 use async_trait::async_trait;
-use sqlx::{Database, Transaction};
+use sqlx::{Connection, Database, Transaction};
 
-pub use crate::application::common::traits::UnitOfWork;
-
-pub struct SqlxUnitOfWork<'a, DB: Database> {
-    transaction: Transaction<'a, DB>,
-}
-
-#[async_trait]
-impl<'a, DB> UnitOfWork for SqlxUnitOfWork<'a, DB>
+pub struct SqlxUnitOfWork<DB>
 where
     DB: Database,
 {
+    conn: DB::Connection,
+    transaction: Option<Transaction<'static, DB>>,
+}
+
+impl<DB> SqlxUnitOfWork<DB>
+where
+    DB: Database,
+{
+    pub fn new(conn: DB::Connection) -> Self {
+        Self {
+            conn,
+            transaction: None,
+        }
+    }
+}
+
+#[async_trait]
+impl<DB> UnitOfWork for SqlxUnitOfWork<DB>
+where
+    DB: Database,
+{
+    type BeginError = sqlx::Error;
     type CommitError = sqlx::Error;
     type RollbackError = sqlx::Error;
 
-    async fn commit(self) -> Result<(), Self::CommitError> {
-        self.transaction.commit().await
+    type Connection<'a> = &'a mut DB::Connection where Self: 'a;
+
+    fn connection(&mut self) -> Self::Connection<'_> {
+        &mut self.conn
     }
 
-    async fn rollback(self) -> Result<(), Self::RollbackError> {
-        self.transaction.rollback().await
+    async fn begin(&'static mut self) -> Result<Self::Connection<'_>, Self::BeginError> {
+        self.transaction = Some(self.conn.begin().await?);
+
+        Ok(self.transaction.as_mut().unwrap())
+    }
+
+    async fn commit(&mut self) -> Result<(), Self::CommitError> {
+        if let Some(transaction) = self.transaction.take() {
+            transaction.commit().await
+        } else {
+            Ok(())
+        }
+    }
+
+    async fn rollback(&mut self) -> Result<(), Self::RollbackError> {
+        if let Some(transaction) = self.transaction.take() {
+            transaction.rollback().await
+        } else {
+            Ok(())
+        }
     }
 }
